7f565d4987370819a03cb51f835ef55490cc77e1
Vladimir Kapanin <will-lizok@mail.ru>
2019-03-05 12:48:15 +0300

    c03

--- src/main/java/jagger/JLoadScenarioProvider.java
+++ src/main/java/jagger/JLoadScenarioProvider.java
@@ -37,158 +37,158 @@ public class JLoadScenarioProvider extends JaggerPropertiesProvider {
         String url = getTestPropertyValue("jagger.load.scenario.tests.url");
 
         //5
-        Integer iterationsNumberForTest1 = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test1.termination.iterations"));
+        Integer iterationsNumberForGetTest = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test1.termination.iterations"));
         //2
-        Integer userNumberForTest1 = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test1.users"));
+        Integer userNumberForGetTest = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test1.users"));
         //3000
-        Integer delayInvocationDurationForTest1 = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test1.delay.invocation.in.milliseconds"));
+        Integer delayInvocationForGetTest = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test1.delay.invocation.in.milliseconds"));
         ///get
-        String pathForTest1 = getTestPropertyValue("jagger.load.scenario.test1.url.path");
+        String pathForGetTest = getTestPropertyValue("jagger.load.scenario.test1.url.path");
 
-        JTestDefinition testDefinitionForTest1 = JTestDefinition
-                .builder(Id.of("JaggerTestDefinition1"), new EndpointsProvider(url))
+        JTestDefinition getTestDefinition = JTestDefinition
+                .builder(Id.of("GetTestDefinition"), new EndpointsProvider(url))
                 .withInvoker(CustomHttpInvokerProvider.nonVerbose())
-                .withQueryProvider(new QueriesProvider(pathForTest1))
+                .withQueryProvider(new QueriesProvider(pathForGetTest))
                 .withLoadBalancer(new RoundRobinLoadBalancer())
                 .addValidator(new ValidatorResponseStatus())
-                .addValidator(new ValidatorResponseUrl(pathForTest1))
+                .addValidator(new ValidatorResponseUrl(pathForGetTest))
                 .addListener(new HeadersCalculator())
                 .addListener(new NotNullInvocationListener())
                 .build();
 
-        JTerminationCriteria terminationCriteriaForTest1 = JTerminationCriteriaIterations
-                .of(IterationsNumber.of(iterationsNumberForTest1), MaxDurationInSeconds.of(maxDurationInSecondsGeneral));
+        JTerminationCriteria getTerminationCriteria = JTerminationCriteriaIterations
+                .of(IterationsNumber.of(iterationsNumberForGetTest), MaxDurationInSeconds.of(maxDurationInSecondsGeneral));
 
-        JLoadProfileUsers loadProfileUsersForTest1 = JLoadProfileUsers
-                .builder(NumberOfUsers.of(userNumberForTest1))
+        JLoadProfileUsers getLoadProfileUser = JLoadProfileUsers
+                .builder(NumberOfUsers.of(userNumberForGetTest))
                 .build();
 
-        JLoadProfileUserGroups loadProfileUserGroupsForTest1 = JLoadProfileUserGroups
-                .builder(loadProfileUsersForTest1)
-                .withDelayBetweenInvocationsInMilliseconds(delayInvocationDurationForTest1)
+        JLoadProfileUserGroups getLoadProfileGroup = JLoadProfileUserGroups
+                .builder(getLoadProfileUser)
+                .withDelayBetweenInvocationsInMilliseconds(delayInvocationForGetTest)
                 .build();
 
-        JLoadTest jLoadTest1 = JLoadTest
-                .builder(Id.of("JaggerLoadTest1"), testDefinitionForTest1, loadProfileUserGroupsForTest1, terminationCriteriaForTest1)
+        JLoadTest getLoadTest = JLoadTest
+                .builder(Id.of("GetLoadTest"), getTestDefinition, getLoadProfileGroup, getTerminationCriteria)
                 .addListener(new CollectThreadsTestListener())
                 .build();
 
-        JParallelTestsGroup jParallelTestsGroupTest1 = JParallelTestsGroup
-                .builder(Id.of("JaggerParallelTestsGroup1"), jLoadTest1)
+        JParallelTestsGroup getTestsGroup = JParallelTestsGroup
+                .builder(Id.of("GetTestsGroup"), getLoadTest)
                 .build();
 
 
         //120
-        Integer terminationDurationForTest2 = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test2.termination.duration.in.seconds"));
+        Integer terminationDurationForXMLTest = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test2.termination.duration.in.seconds"));
         //3
-        Integer userNumberForTest2 = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test2.users"));
+        Integer userNumberForXMLTest = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test2.users"));
         //15000
-        Integer delayInvocationDurationForTest2 = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test2.delay.invocation.in.milliseconds"));
+        Integer delayInvocationForXMLTest = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test2.delay.invocation.in.milliseconds"));
         //20
-        Integer delayStartForTest2 = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test2.delay.start.in.milliseconds"));
+        Integer delayStartForXMLTest = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test2.delay.start.in.milliseconds"));
         ///xml
-        String pathForTest2 = getTestPropertyValue("jagger.load.scenario.test2.url.path");
+        String pathForXMLTest = getTestPropertyValue("jagger.load.scenario.test2.url.path");
 
-        JTestDefinition testDefinitionForTest2 = JTestDefinition
-                .builder(Id.of("JaggerTestDefinition2"), new EndpointsProvider(url))
+        JTestDefinition xmlTestDefinition = JTestDefinition
+                .builder(Id.of("XMLTestDefinition"), new EndpointsProvider(url))
                 .withInvoker(CustomHttpInvokerProvider.nonVerbose())
-                .withQueryProvider(new QueriesProvider(pathForTest2))
+                .withQueryProvider(new QueriesProvider(pathForXMLTest))
                 .withLoadBalancer(new RoundRobinLoadBalancer())
                 .addValidator(new ValidatorResponseStatus())
                 .addValidator(new ValidatorResponseXML())
                 .addListener(new ResponseCalculator())
                 .build();
 
-        JTerminationCriteria terminationCriteriaForTest2 = JTerminationCriteriaDuration.of(DurationInSeconds.of(terminationDurationForTest2));
+        JTerminationCriteria xmlTerminationCriteria = JTerminationCriteriaDuration.of(DurationInSeconds.of(terminationDurationForXMLTest));
 
-        JLoadProfileUsers loadProfileUsersForTest21 = JLoadProfileUsers
+        JLoadProfileUsers xmlLoadProfileUser1 = JLoadProfileUsers
                 .builder(NumberOfUsers.of(1))
                 .build();
-        JLoadProfileUsers loadProfileUsersForTest22 = JLoadProfileUsers
+        JLoadProfileUsers xmlLoadProfileUser2 = JLoadProfileUsers
                 .builder(NumberOfUsers.of(1))
-                .withStartDelayInSeconds(delayStartForTest2)
+                .withStartDelayInSeconds(delayStartForXMLTest)
                 .build();
-        JLoadProfileUsers loadProfileUsersForTest23 = JLoadProfileUsers
+        JLoadProfileUsers xmlLoadProfileUser3 = JLoadProfileUsers
                 .builder(NumberOfUsers.of(1))
-                .withStartDelayInSeconds(delayStartForTest2)
+                .withStartDelayInSeconds(delayStartForXMLTest)
                 .build();
 
-        JLoadProfileUserGroups loadProfileUserGroupsForTest2 = JLoadProfileUserGroups
-                .builder(loadProfileUsersForTest21,loadProfileUsersForTest22,loadProfileUsersForTest23)
-                .withDelayBetweenInvocationsInMilliseconds(delayInvocationDurationForTest2)
+        JLoadProfileUserGroups xmlLoadProfileGroup = JLoadProfileUserGroups
+                .builder(xmlLoadProfileUser1,xmlLoadProfileUser2,xmlLoadProfileUser3)
+                .withDelayBetweenInvocationsInMilliseconds(delayInvocationForXMLTest)
                 .build();
 
-        JLoadTest jLoadTest2 = JLoadTest
-                .builder(Id.of("JaggerLoadTest2"), testDefinitionForTest2, loadProfileUserGroupsForTest2, terminationCriteriaForTest2)
+        JLoadTest xmlLoadTest = JLoadTest
+                .builder(Id.of("XMLLoadTest"), xmlTestDefinition, xmlLoadProfileGroup, xmlTerminationCriteria)
                 .addListener(new CollectThreadsTestListener())
                 .build();
 
-        JParallelTestsGroup jParallelTestsGroupTest2 = JParallelTestsGroup
-                .builder(Id.of("JaggerParallelTestsGroup2"), jLoadTest2)
+        JParallelTestsGroup xmlTestsGroup = JParallelTestsGroup
+                .builder(Id.of("XMLTestsGroup"), xmlLoadTest)
                 .build();
 
 
         //180
-        Integer terminationDurationForTest3 = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test3.termination.duration.in.seconds"));
+        Integer terminationDurationForResponseTest = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test3.termination.duration.in.seconds"));
         //2
-        Integer userNumberForTest3= Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test3.users"));
+        Integer userNumberForResponseTest= Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test3.users"));
         //20000
-        Integer delayInvocationForTest3 = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test3.delay.in.milliseconds.user1"));
+        Integer delayInvocationForResponseTest = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test3.delay.in.milliseconds.user1"));
         //15000
-        Integer delayStartForTest3User2 = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test3.delay.in.milliseconds.user2"));
+        Integer delayStartForResponseTest = Integer.valueOf(getTestPropertyValue("jagger.load.scenario.test3.delay.in.milliseconds.user2"));
         ///response-headers
-        String pathForTest3 = getTestPropertyValue("jagger.load.scenario.test3.url.path");
+        String pathForResponseTest = getTestPropertyValue("jagger.load.scenario.test3.url.path");
 
-        JTestDefinition testDefinitionForTest3 = JTestDefinition
-                .builder(Id.of("JaggerTestDefinition3"), new EndpointsProvider(url))
+        JTestDefinition responseTestDefinition = JTestDefinition
+                .builder(Id.of("ResponseTestDefinition"), new EndpointsProvider(url))
                 .withInvoker(CustomHttpInvokerProvider.nonVerbose())
-                .withQueryProvider(new QueriesProvider(pathForTest3))
+                .withQueryProvider(new QueriesProvider(pathForResponseTest))
                 .withLoadBalancer(new RoundRobinLoadBalancer())
                 .addValidator(new ValidatorResponseStatus())
                 .addValidator(new ValidatorResponseValue())
                 .addListener(new ResponseLengthCalculator())
                 .build();
 
-        JTerminationCriteria terminationCriteriaForTest3User1 = JTerminationCriteriaDuration.of(DurationInSeconds.of(terminationDurationForTest3));
+        JTerminationCriteria responseTerminationCriteriaForUser1 = JTerminationCriteriaDuration.of(DurationInSeconds.of(terminationDurationForResponseTest));
 
-        JTerminationCriteria terminationCriteriaForTest3User2 = JTerminationCriteriaBackground.getInstance();
+        JTerminationCriteria responseTerminationCriteriaForUser2 = JTerminationCriteriaBackground.getInstance();
 
-        JLoadProfileUsers loadProfileUsersForTest3User1 = JLoadProfileUsers
+        JLoadProfileUsers responseLoadProfileUser1 = JLoadProfileUsers
                 .builder(NumberOfUsers.of(1))
                 .build();
 
-        JLoadProfileUsers loadProfileUsersForTest3User2 = JLoadProfileUsers
+        JLoadProfileUsers responseLoadProfileUser2 = JLoadProfileUsers
                 .builder(NumberOfUsers.of(1))
-                .withStartDelayInSeconds(delayStartForTest3User2)
+                .withStartDelayInSeconds(delayStartForResponseTest)
                 .build();
 
-        JLoadProfileUserGroups loadProfileUserGroupsForTest31 = JLoadProfileUserGroups
-                .builder(loadProfileUsersForTest3User1,loadProfileUsersForTest3User2)
-                .withDelayBetweenInvocationsInMilliseconds(delayInvocationForTest3)
+        JLoadProfileUserGroups responseLoadProfileGroup1 = JLoadProfileUserGroups
+                .builder(responseLoadProfileUser1,responseLoadProfileUser2)
+                .withDelayBetweenInvocationsInMilliseconds(delayInvocationForResponseTest)
                 .build();
 
-        JLoadProfileUserGroups loadProfileUserGroupsForTest32 = JLoadProfileUserGroups
-                .builder(loadProfileUsersForTest3User1,loadProfileUsersForTest3User2)
-                .withDelayBetweenInvocationsInMilliseconds(delayInvocationForTest3)
+        JLoadProfileUserGroups responseLoadProfileGroup2 = JLoadProfileUserGroups
+                .builder(responseLoadProfileUser1,responseLoadProfileUser2)
+                .withDelayBetweenInvocationsInMilliseconds(delayInvocationForResponseTest)
                 .build();
 
-        JLoadTest jLoadTest3User1 = JLoadTest
-                .builder(Id.of("JaggerLoadTest31"), testDefinitionForTest3, loadProfileUserGroupsForTest31, terminationCriteriaForTest3User1)
+        JLoadTest responseLoadTestUser1 = JLoadTest
+                .builder(Id.of("ResponseLoadTestUser1"), responseTestDefinition, responseLoadProfileGroup1, responseTerminationCriteriaForUser1)
                 .addListener(new CollectThreadsTestListener())
                 .build();
 
-        JLoadTest jLoadTest3User2 = JLoadTest
-                .builder(Id.of("JaggerLoadTest32"), testDefinitionForTest3, loadProfileUserGroupsForTest32, terminationCriteriaForTest3User2)
+        JLoadTest responseLoadTestUser2 = JLoadTest
+                .builder(Id.of("ResponseLoadTestUser2"), responseTestDefinition, responseLoadProfileGroup2, responseTerminationCriteriaForUser2)
                 .addListener(new CollectThreadsTestListener())
                 .build();
 
-        JParallelTestsGroup jParallelTestsGroupTest3 = JParallelTestsGroup
-                .builder(Id.of("JaggerParallelTestsGroup3"), jLoadTest3User1, jLoadTest3User2)
+        JParallelTestsGroup responseTestsGroup = JParallelTestsGroup
+                .builder(Id.of("ResponseTestsGroup"), responseLoadTestUser1, responseLoadTestUser2)
                 .build();
 
 
 
-        return JLoadScenario.builder(Id.of("testJaggerLoadScenario"), jParallelTestsGroupTest1,jParallelTestsGroupTest2,jParallelTestsGroupTest3)
+        return JLoadScenario.builder(Id.of("testJaggerLoadScenario"), getTestsGroup,xmlTestsGroup,responseTestsGroup)
                 .addListener(new ExampleLoadScenarioListener())
                 .withLatencyPercentiles(Arrays.asList(85D, 90D, 95D))
                 .build();
